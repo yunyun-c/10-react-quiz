cc196f42b7fbc2db05b7c33b7f535f7c2d9c2f04
cassie_c <cassie.nightingale.13.13@gmail.com>
2023-12-21 17:39:18 -0600

    course 192

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 const initialState = {
   questions: [],
@@ -28,7 +31,9 @@ function reducer(state, action) {
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const unmQuestions = questions.length;
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
@@ -42,8 +47,9 @@ function App() {
       <Header />
 
       <Main>
-        <p>1/15</p>
-        <p>Questions</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen unmQuestions={unmQuestions} />}
       </Main>
     </div>
   );
