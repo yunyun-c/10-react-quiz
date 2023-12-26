a0f0150599721266f8653eb48bc7a50e6b7b8634
cassie_c <cassie.nightingale.13.13@gmail.com>
2023-12-26 16:09:34 -0600

    course 199

--- src/components/App.js
+++ src/components/App.js
@@ -57,6 +57,8 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "restart":
+      return { ...initialState, questions: state.questions, status: "ready" };
     default:
       throw new Error("Action unknown");
   }
@@ -116,6 +118,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
